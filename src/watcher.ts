4aa5c4506133719f8a1be761b7e1af38ee547bb6
jfleon <jfleon@nukk.ai>
2024-10-17 17:50:47 +0200

    Added custom collections

--- src/watcher.ts
+++ src/watcher.ts
@@ -2,15 +2,20 @@ import { createStore, produce, SetStoreFunction } from "solid-js/store";
 import { SupabaseClient } from "@supabase/supabase-js";
 
 type GenericClient = {
-  getAll(table: string): Promise<any[]>;
+  get(table: string, query?: () => Promise<any[]>): Promise<any[]>;
 };
+
+type QueryType = [string, string, (() => Promise<any[]>) | undefined];
+
+type TableSelector = string | QueryType;
 
 type ClientProvider<T> = (
   client: T,
+  tablesMap: Map<string, string[]>,
   set: SetStoreFunction<Record<string, any[]>>
 ) => GenericClient;
 
-export const supaConnector: ClientProvider<SupabaseClient> = (client, set) => {
+export const supaConnector: ClientProvider<SupabaseClient> = (client, tablesMap, set) => {
   client
     .channel("schema-db-changes")
     .on(
@@ -20,23 +25,27 @@ export const supaConnector: ClientProvider<SupabaseClient> = (client, set) => {
         schema: "public",
       },
       (payload) => {
+        console.log(payload);
         set(
           produce((state) => {
             console.log(payload);
-            if (payload.eventType === "INSERT") {
-              state[payload.table].push(payload.new);
-            } else if (payload.eventType === "UPDATE") {
-              const idx = state[payload.table].findIndex(
-                (s) => s.id === payload.new.id
-              );
-              if (idx === -1) return;
-              state[payload.table][idx] = payload.new;
-            } else {
-              const idx = state[payload.table].findIndex(
-                (s) => s.id === payload.old.id
-              );
-              if (idx === -1) return;
-              state[payload.table].splice(idx, 1);
+
+            for (const customTable of tablesMap.get(payload.table) ?? []) {
+              if (payload.eventType === "INSERT") {
+                state[customTable].push(payload.new);
+              } else if (payload.eventType === "UPDATE") {
+                const idx = state[customTable].findIndex(
+                  (s) => s.id === payload.new.id
+                );
+                if (idx === -1) return;
+                state[customTable][idx] = payload.new;
+              } else {
+                const idx = state[customTable].findIndex(
+                  (s) => s.id === payload.old.id
+                );
+                if (idx === -1) return;
+                state[customTable].splice(idx, 1);
+              }
             }
           })
         );
@@ -45,25 +54,52 @@ export const supaConnector: ClientProvider<SupabaseClient> = (client, set) => {
     .subscribe();
 
   return {
-    async getAll(table: string) {
-      return (await client.from(table).select()).data!;
+    async get(table: string, query?: () => Promise<any[]>): Promise<any[]> {
+      const init =
+        query || (async () => (await client.from(table).select()).data!);
+      return await init();
     },
   };
 };
 
 export function useWatcher<T>(
   client: T,
-  tables: string[],
+  tables: TableSelector[],
   clientProvider: ClientProvider<T> = supaConnector as any
 ) {
   const [store, setStore] = createStore<Record<string, any[]>>({});
 
-  const initializedClient = clientProvider(client, setStore);
+  let tablesMap = new Map<string, string[]>();
+  for (const tableSelector of tables) {
+    let customTable, table;
+    if (typeof tableSelector !== "string") {
+      customTable = tableSelector[0];
+      table = tableSelector[1];
+    } else {
+      customTable = tableSelector;
+      table = tableSelector;
+    }
+
+    tablesMap.get(table)?.push(customTable) ||
+      tablesMap.set(table, [customTable]);
+  }
+
+  const initializedClient = clientProvider(client, tablesMap, setStore);
 
   setStore(
     produce(async (state) => {
-      for (const table of tables)
-        state[table] = await initializedClient.getAll(table);
+      for (const tableSelector of tables) {
+        let customTable, table, query;
+        if (typeof tableSelector === "string") {
+          customTable = tableSelector;
+          table = customTable;
+        } else {
+          customTable = tableSelector[0];
+          table = tableSelector[1];
+          query = tableSelector[2];
+        }
+        state[customTable] = await initializedClient.get(table, query);
+      }
     })
   );
 
