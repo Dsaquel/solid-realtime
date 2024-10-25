7f1cb1dad10691b0f3800e251de35cab83bcbcd6
Dimitri Donatien <ddonatien@nukk.ai>
2024-10-25 12:44:37 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { postgrestConnector } from "./lib/connector/rowSQL";
+export { prismaConnector } from "./lib/connector/prisma";
 export { supaConnector } from "./lib/connector/supabase";
 export { firestoreConnector } from "./lib/connector/firebase";
 export { useWatcher } from "./lib/watcher";
